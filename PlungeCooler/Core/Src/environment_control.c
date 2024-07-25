66a3dc4fd12e89cc174ae25b35af9f160497b030
owenClayton <owenclayton@shaw.ca>
2024-07-25 14:27:07 -0400

    removed these files from git ignore

---
+++ PlungeCooler/Core/Src/environment_control.c
@@ -0,0 +1,33 @@
+#include "environment_control.h"
+
+#define SHT40_ADDRESS (0x44 << 1)
+
+float getCurrentTRH () {
+        HAL_StatusTypeDef ret;
+	uint8_t data_tx[1] = {0xFD};
+	uint8_t data_rx[6];
+	ret = HAL_I2C_Master_Transmit(&hi2c1, SHT40_ADDRESS, &data_tx, 1, 1);
+	if ( ret != HAL_OK ) {
+	  printf("Error Tx\r\n");
+	}
+	else{
+		//read bytes
+		 HAL_Delay(10);
+		 ret =  HAL_I2C_Master_Receive(&hi2c1, 0x44, (uint8_t*)&data_rx, 6,1000);
+		 if ( ret != HAL_OK ) {
+			 printf("Error Rx\r\n");
+		 }
+		 else{
+			 for(int i = 0; i < 6 ; i++){
+				 printf("data_rx[%i] = %u \n",i,data_rx[i]);
+			 }
+			 float t_ticks = data_rx[0] * 256 + data_rx[1];
+			 float rh_ticks = data_rx[3] * 256 + data_rx[4];
+
+			 float t_degC = -45 + 175 * t_ticks/65535;
+			 float rh_pRH = -6 + 125 * rh_ticks/65535;
+
+			 return t_degC; 
+		 }
+	}
+}
---
+++ PlungeCooler/Core/Src/environment_control.h
@@ -0,0 +1,8 @@
+#include "main.h"
+
+#ifndef SRC_ENVIRONMENT_CONTROL_H
+#define SRC_ENVIRONMENT_CONTROL_H
+
+float getCurrentTRH (){
+
+}
